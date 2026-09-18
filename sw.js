e391e9cc424615d6f7a85c75350e329e66c0d5fd
yoalibeth <yoalimartinez198@gmail.com>
2026-09-18 10:40:07 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE = "pequenas-pistas-v6";
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.webmanifest",
+        "./icons/icon-192.png",
+        "./icons/icon-512.png",
+        "./apple-touch-icon.png"
+      ])
+    )
+  );
+  self.skipWaiting();
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(
+      cached =>
+        cached ||
+        fetch(event.request).then(response => {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put(event.request, copy));
+          return response;
+        })
+    )
+  );
+});
